e424ed43415436f463f4f9edd83977229abe4b0f
Marcos Vinicius Gonçalves de Souza <marcosvinicius_gsouza@outlook.com>
2016-08-13 17:05:09 -0300

    Segundo commit - Teste do ClienteRepository

---
+++ src/test/java/br/com/fabricadeprogramador/repository/TesteClienteRepository.java
@@ -0,0 +1,27 @@
+package br.com.fabricadeprogramador.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.fabricadeprogramador.model.Cliente;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TesteClienteRepository {
+	
+	@Autowired
+	ClienteRepository repository;
+	
+	@Test
+	public void testeSalvar(){
+		Cliente cliente = new Cliente();
+		cliente.setNome("Jão");
+		cliente.setEmail("jao@hotmail.com");
+		cliente.setCpf("999999999");
+		
+		repository.save(cliente);
+	}
+}
